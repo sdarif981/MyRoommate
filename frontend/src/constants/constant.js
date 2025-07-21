a5b6e51505462982442f183ed2fdee3e3ee547cd
Syed Arif <sdarif981@gmail.com>
2025-07-21 18:37:58 +0530

    updated

--- frontend/src/constants/constant.js
+++ frontend/src/constants/constant.js
@@ -1,2 +1,2 @@
-export const USER_API="http://localhost:3000/api/user";
-export const MESSAGE_API="http://localhost:3000/api/message";
+export const USER_API="https://myroommate.onrender.com/api/user";
+export const MESSAGE_API="https://myroommate.onrender.com/api/message";

d78725a02c169d73e20c3ecd5202f9ff19a854a1
Syed Arif <sdarif981@gmail.com>
2025-07-21 19:31:58 +0530

    updated

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -108,7 +108,7 @@ export const login = async (req, res) => {
       .cookie("token", token, {
         httpOnly: true,
         secure: process.env.NODE_ENV === "production",
-        sameSite: "Strict",
+        sameSite: "None",
         maxAge: 24 * 60 * 60 * 1000,
       })
       .json({ message: "Login successful", user: safeUser, success: true });

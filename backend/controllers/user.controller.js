f10f78f9324202e454a7679887efb49da23d13f5
Syed Arif <sdarif981@gmail.com>
2025-07-21 19:45:39 +0530

    updated

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -108,7 +108,7 @@ export const login = async (req, res) => {
       .cookie("token", token, {
         httpOnly: true,
         secure: true,
-        sameSite: "None",
+        sameSite: "none",
         maxAge: 24 * 60 * 60 * 1000,
       })
       .json({ message: "Login successful", user: safeUser, success: true });
